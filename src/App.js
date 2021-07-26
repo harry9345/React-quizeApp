a2cb2f3af9ebbae7c6b5dfc3e91f034cdcfcd704
Hossein Movahed <7905365-harry9345@users.noreply.gitlab.com>
2021-07-26 10:42:37 +0300

    a quize app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const qustions = [
+    {
+      id: 10,
+      questionText: "where is the iran capital",
+      answersOptions: [
+        { answerText: "Tabriz", isCorrect: false },
+        { answerText: "Tehran ", isCorrect: true },
+        { answerText: "Mashtah ", isCorrect: false },
+        { answerText: "Qom", isCorrect: false },
+      ],
+    },
+    {
+      id: 1,
+      questionText: "Who is CEO of Tesla",
+      answersOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Amir Tehrani ", isCorrect: false },
+        { answerText: "Hamed Rajabi ", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+      ],
+    },
+    {
+      id: 2,
+      questionText: "What is  2 * 5 ",
+      answersOptions: [
+        { answerText: "25", isCorrect: false },
+        { answerText: "90 ", isCorrect: false },
+        { answerText: "68 ", isCorrect: false },
+        { answerText: "10", isCorrect: true },
+      ],
+    },
+    {
+      id: 3,
+      questionText: "Who owns the apple",
+      answersOptions: [
+        { answerText: "Jobs", isCorrect: true },
+        { answerText: "khomeini ", isCorrect: false },
+        { answerText: "rafsanjani ", isCorrect: false },
+        { answerText: "google", isCorrect: false },
+      ],
+    },
+  ];
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswersOptionsClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < qustions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  const handleTryAgain = () => {
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowScore(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {showScore ? (
+        <div className="App">
+          <div className="FinalSection">
+            <h1>Score</h1>
+            <p>
+              Your Score is {score} out of {qustions.length}
+            </p>
+            <a onClick={handleTryAgain}> Try Again</a>
+          </div>
+        </div>
+      ) : (
+        <div className="App">
+          <div className="QuestionSection">
+            <div className="QuestionCount">
+              <span>Question {currentQuestion + 1}</span> / {qustions.length}
+            </div>
+            <div className="QuestionText">
+              {qustions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="AnswerSection">
+            {qustions[currentQuestion].answersOptions.map((answer, index) => (
+              <button
+                key={index}
+                onClick={() => handleAnswersOptionsClick(answer.isCorrect)}
+              >
+                {answer.answerText}
+              </button>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
